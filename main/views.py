152478fd3e934b4179da79236462a00c9d88b27d
gangeshwark@gmail.com <24gokul1995>
2015-08-17 07:29:34 +0530

    Deploy

--- main/views.py
+++ main/views.py
@@ -1,11 +1,9 @@
 import json
-from flask import request, session, make_response, jsonify
-from flask.ext.login import login_user
 
-from flask.ext.restless import APIManager
+from flask import request, session, make_response, jsonify
+
 from main import application, db
-from main.models import UsersLogin, BatteryAuditTable, CableTable, CustomerDetails, NewBatteryTable, OtherDetails, \
-    PendingDetails, SwitchPhysicalCheckTable, TaskTable
+from main.models import UsersLogin, TaskTable
 
 __author__ = 'Gangeshwar'
 
@@ -130,25 +128,6 @@ def check():
     return jsonify(session=session['emp_id'])
 
 
-#############################################################################
-# Create the Flask-Restless API manager.
-# manager = APIManager(application, flask_sqlalchemy_db=db)
-
-
-# Create API endpoints, which will be available at /api/<tablename> by
-# default. Allowed HTTP methods can be specified as well.
-# manager.create_api(UsersLogin, methods=['GET', 'POST', 'PUT', 'DELETE'])
-# manager.create_api(BatteryAuditTable, methods=['GET', 'POST', 'PUT', 'DELETE'])
-# manager.create_api(CableTable, methods=['GET', 'POST', 'PUT', 'DELETE'])
-# manager.create_api(CustomerDetails, methods=['GET', 'POST', 'PUT', 'DELETE'])
-# manager.create_api(NewBatteryTable, methods=['GET', 'POST', 'PUT', 'DELETE'])
-# manager.create_api(PendingDetails, methods=['GET', 'POST', 'PUT', 'DELETE'])
-# manager.create_api(SwitchPhysicalCheckTable, methods=['GET', 'POST', 'PUT', 'DELETE'])
-# manager.create_api(TaskTable, methods=['GET', 'POST', 'PUT', 'DELETE'])
-# manager.create_api(OtherDetails, methods=['GET', 'POST', 'PUT', 'DELETE'])
-
-
-#############################################################################
 @application.route('/createdb')
 def create_db():
     db.create_all()
